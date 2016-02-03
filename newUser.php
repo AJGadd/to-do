1e488bafb4594181f57c4b6853521a4994902796
Andrew J. Gadd <andrewgadd1@hotmail.com>
2016-02-03 12:45:50 -0400

    Added an if statement to each file containing a button. If statement checks to see if Post method contains anything before writing to database and redirects back to the original page else

--- newUser.php
+++ newUser.php
@@ -5,40 +5,47 @@ require_once 'SQLcon.php';
     //check if the POST request method has been used
     if($_SERVER["REQUEST_METHOD"] == 'POST'){
 
-        //create variables named $newUser and $newPassword, set it equal to whats stored in the POST array 'newUser' and 'newPassword' elements.
-        $newUser = $_POST['newUser'];
+        if(!empty($_POST['newUser']) && !empty($_POST['newPassword'])){
 
-        $newPassword = $_POST['newPassword'];
+            //create variables named $newUser and $newPassword, set it equal to whats stored in the POST array 'newUser' and 'newPassword' elements.
+            $newUser = $_POST['newUser'];
 
-        //create a variable called $alreadyExistsQuery and use it to store an SQL query
-        $alreadyExistsQuery = "SELECT user, password
-        FROM users
-        WHERE user = '$newUser'";
+            $newPassword = $_POST['newPassword'];
 
-        //run the query using mysqli_query method, passing it the database connection and query as parameters
-        $alreadyExistsResponse = mysqli_query($con,$alreadyExistsQuery);
+            //create a variable called $alreadyExistsQuery and use it to store an SQL query
+            $alreadyExistsQuery = "SELECT user, password
+            FROM users
+            WHERE user = '$newUser'";
 
-        //create a variable called $newUserQuery and use it to store an SQL query
-        $newUserQuery = " INSERT INTO users (user,password)
-                        VALUES ('$newUser', '$newPassword')";
+            //run the query using mysqli_query method, passing it the database connection and query as parameters
+            $alreadyExistsResponse = mysqli_query($con,$alreadyExistsQuery);
 
-        //run the query using mysqli_query method, passing it the database connection and query as parameters.store the results in a variable
-        $newUserResponse = mysqli_query($con,$newUserQuery);
+            //create a variable called $newUserQuery and use it to store an SQL query
+            $newUserQuery = " INSERT INTO users (user,password)
+                            VALUES ('$newUser', '$newPassword')";
 
-        if(mysqli_num_rows($alreadyExistsResponse)==0){
+            //run the query using mysqli_query method, passing it the database connection and query as parameters.store the results in a variable
+            $newUserResponse = mysqli_query($con,$newUserQuery);
+        
+
+            if(mysqli_num_rows($alreadyExistsResponse)==0){
             
-            header("Location:list.php");
-            
-            $_SESSION['user']=$newUser;
+                $_SESSION['user']=$newUser;
 
-            $_SESSION['password']=$newPassword;
+                $_SESSION['password']=$newPassword;
+
+                header("Location:list.php");
+                die();
            
+            }
+            else{
+            
+                echo "Account details already exist";
+            }
         }
-        else{
-            
-            echo "Account details already exist";
-            
 
+        else{
+                header("Location:newUserIndex.php");
         }
         
     }
